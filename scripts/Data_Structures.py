a2b1d172799bda2d0bc267544759db505211ebe3
jschendel <Jeremy.R.Schendel@gmail.com>
2013-12-08 16:09:44 -0600

    Data Structures (syntax update)
    Updated function name syntax to match the recommended python standard.
    Also, syntax relating to None updated (== None to is None).

--- scripts/Data_Structures.py
+++ scripts/Data_Structures.py
@@ -1,160 +1,167 @@
 #!/usr/bin/env python
 '''A ROSALIND bioinformatics script containing useful data structures.'''
 
+
 class SuffixTree(object):
-	'''Creates a suffix tree for the provided word.'''
-	def __init__(self, word):
-		'''Initializes the suffix tree.'''
-		self.nodes = [self.Node(None,0)]
-		self.edges = dict()
-		if type(word) == str:
-			self.addWord(word)
+    '''Creates a suffix tree for the provided word.'''
 
-	class Node(object):
-	    '''Suffix tree node class.'''
-	    def __init__(self, parent, number):
-	        self.parent = parent
-	        self.number = number
-	        self.children = []
+    def __init__(self, word):
+        '''Initializes the suffix tree.'''
+        self.nodes = [self.Node(None, 0)]
+        self.edges = dict()
+        if type(word) == str:
+            self.add_word(word)
 
-	    def addChild(self, child):
-	    	self.children.append(child)
+    class Node(object):
+        '''Suffix tree node class.'''
+        def __init__(self, parent, number):
+            self.parent = parent
+            self.number = number
+            self.children = []
 
-	    def removeChild(self, child):
-	    	self.children.remove(child)
+        def add_child(self, child):
+            self.children.append(child)
 
-	    def updateParent(self, parent):
-	    	self.parent = parent
+        def remove_child(self, child):
+            self.children.remove(child)
 
+        def update_parent(self, parent):
+            self.parent = parent
 
-	def addWord(self, word):
-		'''Add a word to the suffix tree.'''
-		# Check to make sure word ends in '$'.
-		if word[-1] != '$':
-			word += '$'
-		self.word = word
-		self.n = len(self.word)
+    def add_word(self, word):
+        '''Add a word to the suffix tree.'''
+        # Check to make sure word ends in '$'.
+        if word[-1] != '$':
+            word += '$'
+        self.word = word
+        self.n = len(self.word)
 
-		for i in xrange(self.n):
-			parent_node, edge_start, overlap = self.insertPosition(i,self.nodes[0])
+        for i in xrange(self.n):
+            parent_node, edge_start, overlap = self.insert_position(i, self.nodes[0])
 
-			if overlap:
-				p_edge_start, p_edge_end = self.edges[(parent_node.parent.number, parent_node.number)]
-				
-				# Get the edge to insert
-				insert_len = 0
-				while word[edge_start:edge_start+insert_len] == word[p_edge_start:p_edge_start+insert_len]:
-					insert_len += 1
+            if overlap:
+                p_edge_start, p_edge_end = self.edges[(parent_node.parent.number, parent_node.number)]
 
-				# Create a new node for insertion
-				new_node = self.Node(parent_node.parent,len(self.nodes))
-				new_node.addChild(parent_node)
-				self.addNode(parent_node.parent, p_edge_start, p_edge_start + insert_len-1, new_node)
+                # Get the edge to insert
+                insert_len = 0
+                while word[edge_start:edge_start + insert_len] == word[p_edge_start:p_edge_start + insert_len]:
+                    insert_len += 1
 
-				# Update the parent node since a new node is inserted above it
-				del self.edges[(parent_node.parent.number, parent_node.number)]
-				parent_node.parent.removeChild(parent_node)
-				parent_node.updateParent(new_node)
-				self.edges[(parent_node.parent.number, parent_node.number)] = [p_edge_start + insert_len-1, p_edge_end]
-				
-				# Add new child node
-				self.addNode(new_node, edge_start + insert_len-1, self.n)
+                # Create a new node for insertion
+                new_node = self.Node(parent_node.parent, len(self.nodes))
+                new_node.add_child(parent_node)
+                self.add_node(parent_node.parent, p_edge_start, p_edge_start + insert_len - 1, new_node)
 
-			else:
-				# No insertion necessary, just append the new node.
-				self.addNode(parent_node, edge_start, self.n)
+                # Update the parent node since a new node is inserted above it
+                del self.edges[(parent_node.parent.number, parent_node.number)]
+                parent_node.parent.remove_child(parent_node)
+                parent_node.update_parent(new_node)
+                self.edges[(parent_node.parent.number, parent_node.number)] = [p_edge_start + insert_len - 1, p_edge_end]
 
-	def insertPosition(self, start_index, parent_node):
-		'''Determine the location and method to insert a suffix into the suffix tree.'''
-		for child_node in parent_node.children:
-			edge_start, edge_end = self.edges[(parent_node.number,child_node.number)]
-			if self.word[start_index:start_index+edge_end-edge_start] == self.word[edge_start:edge_end]:
-				return self.insertPosition(start_index+edge_end-edge_start, child_node)
+                # Add new child node
+                self.add_node(new_node, edge_start + insert_len - 1, self.n)
 
-			elif self.word[edge_start] == self.word[start_index]:
-				return child_node, start_index,  True
+            else:
+                # No insertion necessary, just append the new node.
+                self.add_node(parent_node, edge_start, self.n)
 
-		return parent_node, start_index, False	
+    def insert_position(self, start_index, parent_node):
+        '''Determine the location and method to insert a suffix into the suffix tree.'''
+        for child_node in parent_node.children:
+            edge_start, edge_end = self.edges[(parent_node.number, child_node.number)]
+            if self.word[start_index:start_index + edge_end - edge_start] == self.word[edge_start:edge_end]:
+                return self.insert_position(start_index + edge_end - edge_start, child_node)
 
-	def addNode(self, parent_node, edge_start, edge_end, child_node=None):
-		'''Adds a node and the associated edge to the suffix tree.'''
-		# Create child node, if necessary
-		if child_node == None:
-			child_node = self.Node(parent_node, len(self.nodes))
-		# Add node to node list
-		self.nodes.append(child_node)
-		# Add child to parent
-		parent_node.addChild(child_node)
-		# Add edge to edge dict
-		self.edges[(parent_node.number,child_node.number)] = [edge_start, edge_end]
+            elif self.word[edge_start] == self.word[start_index]:
+                return child_node, start_index,  True
 
-	def printEdges(self):
-		'''Returns the string representations of the edges.'''
-		return [self.word[i:j] for i,j in self.edges.values()]
+        return parent_node, start_index, False
+
+    def add_node(self, parent_node, edge_start, edge_end, child_node=None):
+        '''Adds a node and the associated edge to the suffix tree.'''
+
+        # Create child node, if necessary
+        if child_node is None:
+            child_node = self.Node(parent_node, len(self.nodes))
+
+        # Add node to node list
+        self.nodes.append(child_node)
+
+        # Add child to parent
+        parent_node.add_child(child_node)
+
+        # Add edge to edge dict
+        self.edges[(parent_node.number, child_node.number)] = [
+            edge_start, edge_end]
+
+    def print_edges(self):
+        '''Returns the string representations of the edges.'''
+        return [self.word[i:j] for i, j in self.edges.values()]
 
 
 class Trie(object):
-	'''Constructs a trie.'''
-	def __init__(self, word=None):
-		self.nodes = [[self.Node('', 1)]]
-		self.edges = []
-		if word != None:
-			self.addWord(word)
+    '''Constructs a trie.'''
 
-	class Node(object):
-		'''Trie node class.'''
-		def __init__(self, prefix, number):
-			self.prefix = prefix
-			self.number = number
-			self.depth = len(prefix)
+    def __init__(self, word=None):
+        self.nodes = [[self.Node('', 1)]]
+        self.edges = []
+        if word is not None:
+            self.add_word(word)
 
-	class Edge(object):
-		'''Trie edge class.'''
-		def __init__(self, letter, par_node, chi_node):
-			self.letter = letter
-			self.parent_node = par_node
-			self.child_node = chi_node
+    class Node(object):
+        '''Trie node class.'''
+        def __init__(self, prefix, number):
+            self.prefix = prefix
+            self.number = number
+            self.depth = len(prefix)
 
-		def getInfo(self):
-			'''Return the edge information compactly.'''
-			return ' '.join(map(str,[self.parent_node,self.child_node,self.letter]))
+    class Edge(object):
+        '''Trie edge class.'''
+        def __init__(self, letter, par_node, chi_node):
+            self.letter = letter
+            self.parent_node = par_node
+            self.child_node = chi_node
 
-	def addWord(self, word):
-		'''Adds a word to the trie.'''
-		if type(word) == list:
-			for w in word:
-				self.addWord(w)
-		else:
-			parent = self.findParent(word)
-			for i in range(len(parent.prefix), len(word)):
-				new_node = self.Node(word[:i+1], self.nodeCount()+1)
-				self.edges.append(self.Edge(word[i], parent.number, self.nodeCount()+1))
-				self.insertNode(new_node)
-				parent = new_node
+        def get_info(self):
+            '''Return the edge information compactly.'''
+            return ' '.join(map(str, [self.parent_node, self.child_node, self.letter]))
 
-	def insertNode(self, node):
-		'''Determine the location to insert the current node.'''
-		if node.depth > self.depth():
-			self.nodes.append([node])
-		else:
-			self.nodes[node.depth].append(node)
+    def add_word(self, word):
+        '''Adds a word to the trie.'''
+        if type(word) == list:
+            for w in word:
+                self.add_word(w)
+        else:
+            parent = self.find_parent(word)
+            for i in range(len(parent.prefix), len(word)):
+                new_node = self.Node(word[:i + 1], self.node_count() + 1)
+                self.edges.append(self.Edge(word[i], parent.number, self.node_count() + 1))
+                self.insert_node(new_node)
+                parent = new_node
 
-	def depth(self):
-		'''Returns the depth of the trie.'''
-		return len(self.nodes) - 1
+    def insert_node(self, node):
+        '''Determine the location to insert the current node.'''
+        if node.depth > self.depth():
+            self.nodes.append([node])
+        else:
+            self.nodes[node.depth].append(node)
 
-	def nodeCount(self):
-		'''Returns the total number of nodes.'''
-		count = 0
-		for trie_depth in self.nodes:
-			count += len(trie_depth)
-		return count
+    def depth(self):
+        '''Returns the depth of the trie.'''
+        return len(self.nodes) - 1
 
-	def findParent(self, word):
-		'''Return the parent node of the word to be inserted.'''
-		for i in range(min(len(word), self.depth()), 0,-1):
-			for node in self.nodes[i]:
-				if word[:i] == node.prefix:
-					return node
+    def node_count(self):
+        '''Returns the total number of nodes.'''
+        count = 0
+        for trie_depth in self.nodes:
+            count += len(trie_depth)
+        return count
 
-		return self.nodes[0][0]
+    def find_parent(self, word):
+        '''Return the parent node of the word to be inserted.'''
+        for i in range(min(len(word), self.depth()), 0, -1):
+            for node in self.nodes[i]:
+                if word[:i] == node.prefix:
+                    return node
+
+        return self.nodes[0][0]
