8b00feb4c9b8ad4d7923e828d01870ba081e5adf
jschendel <Jeremy.R.Schendel@gmail.com>
2013-12-08 16:10:50 -0600

    Introduction to Pattern Matching (syntax updated)
    Updated syntax to match new syntax in the Trie class of the Data
    Structures script.

--- 054_TRIE.py
+++ 054_TRIE.py
@@ -11,12 +11,11 @@ URL: http://rosalind.info/problems/trie/
 from scripts import Trie
 
 with open('data/rosalind_trie.txt') as input_data:
-	dna = [line.strip() for line in input_data.readlines()]
+    dna = [line.strip() for line in input_data.readlines()]
 
 # The heavy lifting is done by the Trie class in the Data Structures script.
-myTrie = Trie(dna)
-adjacency_list = [edge.getInfo() for edge in myTrie.edges]
+adjacency_list = [edge.get_info() for edge in Trie(dna).edges]
 
 print '\n'.join(adjacency_list)
 with open('output/054_TRIE.txt', 'w') as output_file:
-	output_file.write('\n'.join(adjacency_list))
+    output_file.write('\n'.join(adjacency_list))
