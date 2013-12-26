48bf430f99f4674959b3a5f4e4368895daebea24
jschendel <Jeremy.R.Schendel@gmail.com>
2013-12-26 12:38:15 -0600

    Script: Scoring Matrices
    A script containing classes of various scoring matrices.

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,8 +3,9 @@
 Scripts for functions common to multiple ROSALIND bioinformatics problems.
 '''
 
-from FASTA import ReadFASTA
-from Protein_Dictionaries import ProteinDictDNA, ProteinDictRNA, ProteinWeightDict
-from DNA_RNA_Operations import DNA_to_RNA, RNA_to_DNA, ReverseComplementDNA, ReverseComplementRNA, HammingDistance
 from Data_Structures import SuffixTree, Trie
+from DNA_RNA_Operations import DNA_to_RNA, RNA_to_DNA, HammingDistance, ReverseComplementDNA, ReverseComplementRNA
+from FASTA import ReadFASTA
 from Newick_Trees import Newick, WeightedNewick
+from Protein_Dictionaries import ProteinDictDNA, ProteinDictRNA, ProteinWeightDict
+from scoring_matrices import BLOSUM62, PAM250
