e06040782dc2c3a340efec3c0cc9559613736bf6
jschendel <Jeremy.R.Schendel@gmail.com>
2014-02-09 23:14:56 -0600

    DNA and RNA Operations (syntax)
    Standardized spacing.

--- scripts/DNA_RNA_Operations.py
+++ scripts/DNA_RNA_Operations.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python
 '''ROSALIND bioinformatics scripts that returns that operate on DNA and RNA.'''
 
-from string import maketrans 
-
-# Kind of pointless, as it's so simple.
-def DNA_to_RNA(dna):
-	'''Translates DNA to RNA'''
-	return dna.replace('T', 'U')
-
-# Kind of pointless, as it's so simple.
-def RNA_to_DNA(rna):
-	'''Translates RNA to DNA'''
-	return rna.replace('U', 'T')
+from operator import ne
+from string import maketrans
 
 
-def ReverseComplementDNA(nucleic_acid):
-	'''Returns the reverse complement of a given DNA strand.'''
-	nucleotide = 'ATCG'
-	complement = 'TAGC'
-	transtab = maketrans(nucleotide, complement)
-	
-	return nucleic_acid.translate(transtab)[::-1].lstrip()
+def DNA_to_RNA(dna):  # Kind of pointless, as it's so simple.
+    '''Translates DNA to RNA'''
+    return dna.replace('T', 'U')
 
-def ReverseComplementRNA(nucleic_acid):
-	'''Returns the reverse complement of a given RNA strand.'''
-	nucleotide = 'AUCG'
-	complement = 'UAGC'
-	transtab = maketrans(nucleotide, complement)
-	
-	return nucleic_acid.translate(transtab)[::-1].lstrip()
+
+def RNA_to_DNA(rna):  # Kind of pointless, as it's so simple.
+    '''Translates RNA to DNA'''
+    return rna.replace('U', 'T')
+
+
+def ReverseComplementDNA(dna):
+    '''Returns the reverse complement of a given DNA strand.'''
+    nucleotide = 'ATCG'
+    complement = 'TAGC'
+    transtab = maketrans(nucleotide, complement)
+
+    return dna.translate(transtab)[::-1].lstrip()
+
+
+def ReverseComplementRNA(rna):
+    '''Returns the reverse complement of a given RNA strand.'''
+    nucleotide = 'AUCG'
+    complement = 'UAGC'
+    transtab = maketrans(nucleotide, complement)
+
+    return rna.translate(transtab)[::-1].lstrip()
+
 
 def HammingDistance(seq1, seq2):
-    'Return the Hamming distance between equal-length sequences.'
+    'Returns the Hamming distance between equal-length sequences.'
     if len(seq1) != len(seq2):
         raise ValueError('Undefined for sequences of unequal length.')
-    return sum(ch1 != ch2 for ch1, ch2 in zip(seq1, seq2))
+    return sum(map(ne, seq1, seq2))
