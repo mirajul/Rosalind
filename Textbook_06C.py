8cd061f13117a914495810ad9f3aabd30056022b
jschendel <Jeremy.R.Schendel@gmail.com>
2014-02-19 23:51:41 -0600

    2-Break Distance Problem (refactored code)
    Renamed variables to match Python conventions.  BFS component is now
    purely in terms of sets, instead of a mixture of lists and sets.

--- Textbook_06C.py
+++ Textbook_06C.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-A solution to a code challenges that accompanies Bioinformatics Algorithms: An Active-Learning Approach by Phillip Compeau & Pavel Pevzner.
+A solution to a code challenges that accompanies Bioinformatics Algorithms: An Active-nearning Approach by Phillip Compeau & Pavel Pevzner.
 The textbook is hosted on Stepic and the problem is listed on ROSALIND under the Textbook Track.
 
 Problem Title: 2-Break Distance Problem
@@ -17,23 +17,23 @@ def two_break_dist(P, Q):
     # Construct the break point graph of P and Q.
     graph = defaultdict(list)
     for perm_cycle in P+Q:
-        L = len(perm_cycle)
-        for i in xrange(len(perm_cycle)):
+        n = len(perm_cycle)
+        for i in xrange(n):
             # Add the edge between consecutive items (both orders since the breakpoint graph is undirected).
-            # Note: Modulo L in the higher index for the edge between the last and first elements.
-            graph[perm_cycle[i]].append(-1*perm_cycle[(i+1) % L])
-            graph[-1*perm_cycle[(i+1) % L]].append(perm_cycle[i])
+            # Note: Modulo n in the higher index for the edge between the last and first elements.
+            graph[perm_cycle[i]].append(-1*perm_cycle[(i+1) % n])
+            graph[-1*perm_cycle[(i+1) % n]].append(perm_cycle[i])
 
     # BFS to find the number of connected components in the breakpoint graph.
     component_count = 0
     remaining = set(graph.keys())
-    while len(remaining) > 0:
+    while remaining:
         component_count += 1
-        queue = [remaining.pop()]  # Components are cyclic, so starting point is unimportant.
+        queue = {remaining.pop()}  # Components are cyclic, so starting point is unimportant.
         while queue:
-            current = queue.pop(0)
-            queue += filter(lambda node: node in remaining, graph.get(current, []))
-            remaining -= set(queue)  # Overkill, but it's nice and concise!
+            current = queue.pop()
+            queue |= {node for node in graph[current] if node in remaining}
+            remaining -= queue  # Overkill, but it's nice and concise!
 
     # Theorem: d(P,Q) = blocks(P,Q) - cycles(P,Q)
     return sum(map(len,P)) - component_count
