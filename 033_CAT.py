84e0fd57ea1b95a5564eb9de6eaace5f286b85b8
jschendel <Jeremy.R.Schendel@gmail.com>
2014-03-06 01:48:00 -0600

    Catalan Numbers and RNA Secondary Structures (refactored)
    Modified the design to be in terms of functions.  Cleaned up unnecessary
    if/else statements.  Converted loops to list comprehensions.

--- 033_CAT.py
+++ 033_CAT.py
@@ -10,42 +10,49 @@ URL: http://rosalind.info/problems/cat/
 
 from scripts import ReadFASTA
 
-def Noncrossing(rna):
-	'''Returns the number of noncrossing bonding graphs for a given RNA sequence.'''
-	if len(rna) <= 2:
-		# We only send valid rna matchings, so this return is ok.
-		return 1
-	else:
-		# If we've already computed the value, return it!
-		if rna in noncross_dict:
-			return noncross_dict[rna]
-		# Otherwise, calculate the values.
-		else:
-			subintervals = []
-			for i in xrange(1, len(rna),2):
-				if rna[0] == matchings[rna[i]] and check_subinterval(rna[1:i]):
-					subintervals.append([rna[1:i],rna[i+1:]])
 
-			if subintervals == []:
-				# If we didn't find any subintervals, there are no possible noncrossing matchings.
-				noncross_dict[rna] = 0
-			else:
-				# Reduce the problem to noncrossing matchings over the substrings.
-				noncross_dict[rna] = sum([Noncrossing(subint[0])*Noncrossing(subint[1]) for subint in subintervals]) % 1000000
-			
-			return noncross_dict[rna]
+def noncrossing_perfect_bondings(rna):
+    '''Returns the number of noncrossing perfect bondings for the given RNA sequence'''
+    # Initialize dictionaries.
+    bonding = {'A':'U', 'U':'A', 'C':'G', 'G':'C'}
+    noncrossing_bondings = {}
+
+    def count_noncrossing(rna):
+        '''Recursively computes the number of noncrossing perfect bondings for a given RNA sequence'''
+        if len(rna) <= 2:  # We only send valid rna matchings, so this return is ok.
+            return 1
+        elif rna in noncrossing_bondings:  # If we've already computed the value, return it!
+            return noncrossing_bondings[rna]
+
+        # Determine valid locations where the RNA can be split while maintaining necessary noncrossing conditions.
+        splits = [i for i in xrange(1, len(rna), 2) if rna[0] == bonding[rna[i]] and check_subinterval(rna[1:i])]
+
+        # Reduce the problem to noncrossing matchings over the subintervals.
+        noncrossing_bondings[rna] = sum([count_noncrossing(rna[1:i])*count_noncrossing(rna[i+1:]) for i in splits]) % 1000000
+
+        return noncrossing_bondings[rna]
+
+    return count_noncrossing(rna)
+
 
 def check_subinterval(subint):
-	'''Checks if a given subinterval has the same number of matching nucleotides.'''
-	N = [subint.count(nucleotide) for nucleotide in 'AUCG']
-	if N[0] == N[1] and N[2] == N[3]:
-		return True
-	return False
+    '''Checks if a given subinterval has the same number of matching nucleotides.'''
+    N = [subint.count(nucleotide) for nucleotide in 'AUCG']
+    return N[0] == N[1] and N[2] == N[3]  # Necessary for a noncrossing perfect bonding.
 
-rna = ReadFASTA('data/rosalind_cat.txt')[0][1]
-noncross_dict = {}
-matchings = {'A':'U', 'U':'A', 'C':'G', 'G':'C'}
-noncross = Noncrossing(rna)
-print noncross
-with open('output/033_CAT.txt', 'w') as output_file:
-	output_file.write(str(noncross))
+
+def main():
+    '''Main call. Reads, runs, and saves problem specific data.'''
+    # Parse the input data.
+    rna = ReadFASTA('data/rosalind_cat.txt')[0][1]
+
+    # Get the number of noncrossing perfect bondings.
+    noncrossing = str(noncrossing_perfect_bondings(rna))
+
+    # Print and save the answer.
+    print noncrossing
+    with open('output/033_CAT.txt', 'w') as output_file:
+        output_file.write(str(noncrossing))
+
+if __name__ == '__main__':
+    main()
