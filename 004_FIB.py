f66476b2738f6ff611c4ed984fb302dc20d0df7b
jschendel <Jeremy.R.Schendel@gmail.com>
2014-07-25 19:21:43 -0600

    Rabbits and Recurrence Relations
    Refactored code.  Same functionality as before.

--- 004_FIB.py
+++ 004_FIB.py
@@ -8,24 +8,30 @@ Rosalind #: 004
 URL: http://rosalind.info/problems/fib/
 '''
 
-def Fib(n,k):
-    '''The total number of rabbit pairs that will be present 
-    after n months if we begin with 1 pair and in each generation, 
-    every pair of reproduction-age rabbits produces a litter of k rabbit pairs
-    ''' 
-    if n < 1:
-        return 0
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    else:
-        return Fib(n-1,k) + k*Fib(n-2, k)
 
-with open('data/rosalind_fib.txt') as input_data:
-	n,k = map(int, input_data.read().split())
+def fib_rabbits(n,k):
+    '''Returns the number of rabbits present after n generations with litters of k pairs.'''
+    rabbits = [0,1]
+    for i in xrange(n-1):
+        rabbits[i % 2] = rabbits[(i-1) % 2] + k*rabbits[i % 2]
 
-rabbits = str(Fib(n,k))
-print rabbits
-with open('output/004_FIB.txt', 'w') as output_data:
-    output_data.write(rabbits)
+    return rabbits[n % 2]
+
+
+def main():
+    '''Main call. Parses, runs, and saves problem specific data.'''
+    # Read the input data.
+    with open('data/rosalind_fib.txt') as input_data:
+        n, k = map(int, input_data.read().strip().split())
+
+    # Get the number of rabbits.
+    rabbits = str(fib_rabbits(n,k))
+
+    # Print and save the answer.
+    print rabbits
+    with open('output/004_FIB.txt', 'w') as output_data:
+        output_data.write(rabbits)
+
+
+if __name__ == '__main__':
+    main()
