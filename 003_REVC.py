b2da93c04367d890ec72309a3088f4ba7e05a730
jschendel <Jeremy.R.Schendel@gmail.com>
2014-07-25 19:21:30 -0600

    Complementing a Strand of DNA
    Refactored code.  Same functionality as before.

--- 003_REVC.py
+++ 003_REVC.py
@@ -8,15 +8,28 @@ Rosalind #: 003
 URL: http://rosalind.info/problems/revc/
 '''
 
-from string import maketrans 
+from string import maketrans
 
-with open('data/rosalind_revc.txt') as input_data:
-	dna = input_data.read()
 
-nucleotide = 'ATCG'
-complement = 'TAGC'
-transtab = maketrans(nucleotide, complement)
-dna_reverse_complement = dna.translate(transtab)[::-1].lstrip()
+def reverse_complement_dna(dna):
+    '''Returns the reverse complement of the given DNA strand.'''
+    transtab = maketrans('ATCG', 'TAGC')
+    return dna.translate(transtab)[::-1]
 
-with open('output/003_REVC.txt', 'w') as output_data:
-	output_data.write(dna_reverse_complement)
+
+def main():
+    '''Main call. Parses, runs, and saves problem specific data.'''
+    # Read the input data.
+    with open('data/rosalind_revc.txt') as input_data:
+        dna = input_data.read().strip()
+
+    # Get the reverse complement.
+    rev_comp = reverse_complement_dna(dna)
+
+    # Print and save the answer.
+    print rev_comp
+    with open('output/003_REVC.txt', 'w') as output_data:
+        output_data.write(rev_comp)
+
+if __name__ == '__main__':
+    main()
