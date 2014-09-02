284c77df0996f2226e672051bbc6feb26e787335
jschendel <Jeremy.R.Schendel@gmail.com>
2014-09-02 00:13:27 -0600

    Frequent Words with Mismatches and Reverse Complements Problem (Refactor)
    Refactored code for increased readability and performance.

--- Textbook_01H.py
+++ Textbook_01H.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-'''
+"""
 A solution to a code challenges that accompanies Bioinformatics Algorithms: An Active-Learning Approach by Phillip Compeau & Pavel Pevzner.
 The textbook is hosted on Stepic and the problem is listed on ROSALIND under the Textbook track.
 
@@ -7,27 +7,46 @@ Problem Title: Frequent Words with Mismatches and Reverse Complements Problem
 Chapter #: 01
 Problem ID: H
 URL: http://rosalind.info/problems/1h/
-'''
+"""
 
-from scripts import ReverseComplementDNA as RevComp
-from Textbook_01G import MismatchList
+from collections import defaultdict
+from scripts import ReverseComplementDNA as rev_comp
+from Textbook_01G import kmer_mismatches
 
-with open('data/textbook/rosalind_1h.txt') as input_data:
-	dna, [k, d] = [line.strip() if index == 0 else map(int, line.strip().split()) for index, line in enumerate(input_data.readlines())]
 
-# Use a dictionary to count the occurence of each k-mer and its reverse complement with up to d mismatches on each.
-mismatch_dict = {}
-for i in xrange(len(dna)-k+1):
-	for kmer in MismatchList(dna[i:i+k], d)+MismatchList(RevComp(dna[i:i+k]), d):
-		if kmer in mismatch_dict:
-			mismatch_dict[kmer] += 1
-		else:
-			mismatch_dict[kmer] = 1
+def freq_words_with_mm_and_rev_comp(seq, k, d):
+    """Returns all most frequent k-mers with up to d mismatches in the dna sequence seq."""
+    # Frequency analysis so we don't generate mismatches for the same k-mer more than once.
+    kmer_freq = defaultdict(int)
+    for i in xrange(len(seq)-k+1):
+        kmer_freq[seq[i:i+k]] += 1
+        kmer_freq[rev_comp(seq[i:i+k])] += 1
 
-# Computing the maximum value is somewhat time consuming to repeat, so only do it once!
-max_val = max(mismatch_dict.values())
-kmers = [item[0] for item in mismatch_dict.items() if item[1] == max_val]
+    # Get all of the mismatches for each unique k-mer in the sequence, appearing freq times.
+    mismatch_count = defaultdict(int)
+    for kmer, freq in kmer_freq.iteritems():
+        for mismatch in kmer_mismatches(kmer, d):
+            mismatch_count[mismatch] += freq
 
-print ' '.join(kmers)
-with open('output/textbook/Textbook_01H.txt', 'w') as output_data:
-	output_data.write(' '.join(kmers))
+    # Computing the maximum value is somewhat time consuming to repeat, so only do it once!
+    max_count = max(mismatch_count.values())
+    return sorted([kmer for kmer, count in mismatch_count.iteritems() if count == max_count])
+
+
+def main():
+    """Main call. Parses, runs, and saves problem specific data."""
+    # Read the input data.
+    with open('data/textbook/rosalind_1h.txt') as input_data:
+        seq = input_data.readline().strip()
+        k, d = map(int, input_data.read().strip().split())
+
+    # Get the most frequent k-mers with up to d mismatches.
+    most_freq_kmers = freq_words_with_mm_and_rev_comp(seq, k, d)
+
+    # Print and save the answer.
+    print ' '.join(most_freq_kmers)
+    with open('output/textbook/Textbook_01H.txt', 'w') as output_data:
+        output_data.write(' '.join(most_freq_kmers))
+
+if __name__ == '__main__':
+    main()
