018797c2b12cfd080d121bfbca10b6a4e59b92bb
jschendel <Jeremy.R.Schendel@gmail.com>
2014-09-02 00:08:41 -0600

    Counting Point Mutations (refactor)
    Refactored code for readability and reusability.  Improved performance
    with imap.

--- 006_HAMM.py
+++ 006_HAMM.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
-'''
+"""
 A solution to a ROSALIND bioinformatics problem.
 
 Problem Title: Counting Point Mutations
 Rosalind ID: HAMM
 Rosalind #: 006
 URL: http://rosalind.info/problems/hamm/
-'''
+"""
 
-from scripts import HammingDistance
+from itertools import imap
+from operator import ne
 
-with open('data/rosalind_hamm.txt') as input_data:
-	s, t = [line.rstrip('\n') for line in input_data.readlines()]
 
-count = HammingDistance(s,t)
+def hamming_distance(seq1, seq2):
+    'Returns the Hamming distance between equal-length sequences.'
+    if len(seq1) != len(seq2):
+        raise ValueError('Undefined for sequences of unequal length.')
+    return sum(imap(ne, seq1, seq2))
 
-print count
-with open('output/006_HAMM.txt', 'w') as output_data:
-	output_data.write(str(count))
+
+def main():
+    """Main call. Parses, runs, and saves problem specific data."""
+    # Read the input data.
+    with open('data/rosalind_hamm.txt') as input_data:
+        seq1, seq2 = [line.strip() for line in input_data]
+
+    # Get the Hamming Distance.
+    h_dist = str(hamming_distance(seq1,seq2))
+
+    # Print and save the answer.
+    print h_dist
+    with open('output/.txt', 'w') as output_data:
+        output_data.write(h_dist)
+
+if __name__ == '__main__':
+    main()
