12bcebc9cfd802ecb23c6bb3cb72c043c711a8d8
jschendel <Jeremy.R.Schendel@gmail.com>
2014-11-09 14:30:47 -0700

    Translating RNA into Protein
    Refactored code.  Updated to use new protein translation script.

--- 008_PROT.py
+++ 008_PROT.py
@@ -1,27 +1,43 @@
 #!/usr/bin/env python
-'''
+"""
 A solution to a ROSALIND bioinformatics problem.
 
 Problem Title: Translating RNA into Protein
 Rosalind ID: PROT
 Rosalind #: 008
 URL: http://rosalind.info/problems/prot/
-'''
+"""
 
-from scripts import ProteinDictRNA
+from itertools import imap, takewhile
+from scripts import ProteinTranslator
 
-with open('data/rosalind_prot.txt') as input_data:
-	s = input_data.read().rstrip('\n')
 
-# Dictionary translating RNA to Protein
-rna_dict = ProteinDictRNA()
+def rna_to_protein(rna):
+    """Translates RNA to Protein."""
+    # Load the translator and create a lambda function to check for the stop codon.
+    translate = ProteinTranslator()
+    not_stop = lambda rna_base: translate.rna_to_protein(rna_base) != 'Stop'
 
-s_protein = ''
-for i in range(0,len(s),3):
-    if rna_dict[s[i:i+3]] != 'Stop':
-        s_protein+= rna_dict[s[i:i+3]]
+    # Generate a list of all rna triples up to the stop codon, then translate to protein.
+    non_stop_rna = takewhile(not_stop, (rna[3*i:3*i+3] for i in xrange(len(rna)/3)))
+    protein_list = imap(translate.rna_to_protein, non_stop_rna)
 
-print s_protein
+    return ''.join(protein_list)
 
-with open('output/008_PROT.txt', 'w') as output_data:
-	output_data.write(s_protein)
+
+def main():
+    """Main call. Parses, runs, and saves problem specific data."""
+    # Read the input data.
+    with open('data/rosalind_prot.txt') as input_data:
+        s = input_data.read().strip()
+
+    # Get the translated protein.
+    protein_output = rna_to_protein(s)
+
+    # Print and save the answer.
+    print protein_output
+    with open('output/008_PROT.txt', 'w') as output_data:
+        output_data.write(protein_output)
+
+if __name__ == '__main__':
+    main()
