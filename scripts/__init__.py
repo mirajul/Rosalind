af2f61b3a0d52b131b4339318796a2df96a3cc0e
jschendel <Jeremy.R.Schendel@gmail.com>
2014-11-09 14:29:41 -0700

    Scripts: Protein Translation
    Updated script for protein translation.  Combined everything into a
    single class.

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -9,6 +9,7 @@ from FASTA import ReadFASTA
 from generalized_suffix_tree import GeneralizedSuffixTree
 from Newick_Trees import Newick, WeightedNewick
 from Protein_Dictionaries import ProteinDictDNA, ProteinDictRNA, ProteinWeightDict
+from protein_scripts import ProteinTranslator
 from scoring_matrices import BLOSUM62, PAM250
 from suffix_array_to_tree import SuffixArrayToTree
 from trie import Trie
